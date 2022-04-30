9dc4de41800074832c8645dde163195bf0f68933
Scott Ashley <burnbright987@gmail.com>
2022-04-30 11:03:01 -0400

    set up html

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,13 +1,18 @@
+var timer = document.getElementById("timer");
+var timeLeft = document.getElementById("timeLeft");
+var timesUp = document.getElementById("timesUp");
+
+
 var questionArr = [
     {
         question: "Commonly used data types DO NOT include:",
-        choices: ["a. strings", "b. booleans", "c. alerts", "d. numbers"],
-        answer: "c. alerts"
+        choices: ["1. strings", "2. booleans", "3. alerts", "4. numbers"],
+        answer: "4. alerts"
     },
     {
         question: "String values must be enclosed within _____ when being assigned to variables.",
-        choices: ["1. commas", "2. curly brackets", "3. quotes", "4. parenthesis"],
-        answer: "4. quotes"
+        choices: ["1. commas", "2. curly brackets", "3. quotes", "4. parentheses"],
+        answer: "3. quotes"
     },
     {
         question: "Arrays in JavaScript can be used to store:",
@@ -16,13 +21,27 @@ var questionArr = [
     },
     {
         question: "The condition of an if/else statement is enclosed with:",
-        choices: ["1. quotes", "2. curly brackets", "3. parenthesis", "4. square brackets"],
-        answer: "3. parenthesis"
+        choices: ["1. quotes", "2. curly brackets", "3. parentheses", "4. square brackets"],
+        answer: "3. parentheses"
     },
     {
         question: "Inside which HTML element do we put the JavaScript?",
         choices: ["1. <script>", "2. <js>", "3. <javascript>", "4. <link>"],
         answer: "1. <script>"
     },
-    
-]
+    {
+        question: "What does DOM stand for?",
+        choices: ["1. Data Object Modifier", "2. Document Object Model", "3. Dangerous Orb Mechanics", "4. Document Order Master"],
+        answer: "2. Document Object Model"
+    },
+    {
+        question: "How do you add a comment in a JavaScript?",
+        choices: ["1. //This is a comment", "2. <!--This is a comment-->", "3. 'This is a comment", "4. * This is a comment *"],
+        answer: "1. //This is a comment"
+    },
+    {
+        question: "Who invented JavaScript?",
+        choices: ["1. Douglas Crockford", "2. Sheryl Sandberg", "3. Brendan Eich", "4. Ben Javascript"],
+        answer: "3. Brendan Eich"
+    }
+];
