7900dfafc46ac8238973c4fa50cd99a757165eed
Scott Ashley <burnbright987@gmail.com>
2022-04-30 09:59:46 -0400

    first commit

---
+++ assets/scripts/script.js
@@ -0,0 +1,28 @@
+var questionArr = [
+    {
+        question: "Commonly used data types DO NOT include:",
+        choices: ["a. strings", "b. booleans", "c. alerts", "d. numbers"],
+        answer: "c. alerts"
+    },
+    {
+        question: "String values must be enclosed within _____ when being assigned to variables.",
+        choices: ["1. commas", "2. curly brackets", "3. quotes", "4. parenthesis"],
+        answer: "4. quotes"
+    },
+    {
+        question: "Arrays in JavaScript can be used to store:",
+        choices: ["1. numbers and strings", "2. other arrays", "3. booleans", "4. all of the above"],
+        answer: "4. all of the above"
+    },
+    {
+        question: "The condition of an if/else statement is enclosed with:",
+        choices: ["1. quotes", "2. curly brackets", "3. parenthesis", "4. square brackets"],
+        answer: "3. parenthesis"
+    },
+    {
+        question: "Inside which HTML element do we put the JavaScript?",
+        choices: ["1. <script>", "2. <js>", "3. <javascript>", "4. <link>"],
+        answer: "1. <script>"
+    },
+    
+]
